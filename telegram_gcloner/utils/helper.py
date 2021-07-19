6feee6a7e83393bd34eae4f2efadbcc4865e864f
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-07-19 21:47:43 +0600

    Update helper.py

--- telegram_gcloner/utils/helper.py
+++ telegram_gcloner/utils/helper.py
@@ -35,7 +35,7 @@ def parse_folder_id_from_url(url):
 
 def alert_users(context, user_info, warning_message, text):
     mention_html_user = mention_html(user_info.id, html.escape(user_info.full_name))
-    message = '🤔 Suspicious behaviour from user {} {}: {} {}.'.format(
+    message = '🤔 এই ব্যবহারকারীর আচরণ সন্দেহজনক {} {}: {} {}.'.format(
         mention_html_user,
         user_info.id,
         warning_message,
